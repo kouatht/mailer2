5c7ad96810eaef571f9cce3bd39f7ffb9d7eeafb
Kohei Ouchi <km0810jtp@gmail.com>
2018-12-24 13:57:40 +0000

    打ち込んだ内容の保持,1文字以上140文字以下,エラーメッセージの表示,を追加

--- app/helpers/kblos_helper.rb
+++ app/helpers/kblos_helper.rb
@@ -1,5 +1,5 @@
 module KblosHelper
-    def choose_new_or_edit
+  def choose_new_or_edit
     if action_name == 'new' || action_name == 'confirm'
       confirm_kblos_path
     elsif action_name == 'edit'
