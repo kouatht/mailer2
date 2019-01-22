1fc7ab310467db86645d77a844b21fafdd6c941c
Kohei Ouchi <km0810jtp@gmail.com>
2019-01-22 17:24:38 +0900

    letter_openerを本番環境でも設定

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" 
 
 end
 
